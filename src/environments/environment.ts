72736b5f128adf2e09f0dcf3b4c875cdd54831b8
Divya Patil <divyaptl1029@gmail.com>
2024-05-04 18:29:29 +0530

    Project Complted

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const Environment ={
+     firebaseConfig : {
+      apiKey: "AIzaSyCxg2N0iMyEHW4Me6KW6hH1iwrEqIs3xJY",
+      authDomain: "hudson-f5a5e.firebaseapp.com",
+      projectId: "hudson-f5a5e",
+      storageBucket: "hudson-f5a5e.appspot.com",
+      messagingSenderId: "730499459531",
+      appId: "1:730499459531:web:07c13f9f3fe88c09322e4e",
+      measurementId: "G-QXM324ETGV"
+      }
+}
